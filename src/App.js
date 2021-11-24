e4e6455eee1ee0ea61571cea31000ef9aed6868f
jericoxnavarro <93170504+JericoK@users.noreply.github.com>
2021-11-24 11:42:55 +0800

    feat: change password

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import HomeScreen from './screens/HomeScreen';
-import ApplicationScreen from './screens/ApplicationScreen';
-import TrackApplicationScreen from './screens/TrackApplicationScreen';
-import AdminLoginScreen from './screens/AdminLoginScreen';
-import AdminDashboardScreen from './screens/AdminDashboardScreen';
-import AdminForgotPasswordScreen from './screens/AdminForgotPasswordScreen';
-import AdminChangePasswordScreen from './screens/AdminChangePasswordScreen';
-import ChangePasswordScreen from './screens/ChangePasswordScreen';
-import ModelListingScreen from './screens/ModelListingScreen';
-import FaqScreen from './screens/FaqScreen';
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import ApplicationScreen from "./screens/ApplicationScreen";
+import TrackApplicationScreen from "./screens/TrackApplicationScreen";
+import AdminLoginScreen from "./screens/AdminLoginScreen";
+import AdminDashboardScreen from "./screens/AdminDashboardScreen";
+import AdminForgotPasswordScreen from "./screens/AdminForgotPasswordScreen";
+import AdminChangePasswordScreen from "./screens/AdminChangePasswordScreen";
+import ChangePasswordScreen from "./screens/ChangePasswordScreen";
+import ModelListingScreen from "./screens/ModelListingScreen";
+import FaqScreen from "./screens/FaqScreen";
+import ResetPasswordScreen from "./screens/ResetPasswordScreen";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import UserForm from './components/admin_forms/UserForm';
+import UserForm from "./components/admin_forms/UserForm";
 
 function App() {
   return (
     <Router>
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/application' component={ApplicationScreen} exact />
-          <Route path='/track' component={TrackApplicationScreen} exact />
+      <Route path="/" component={HomeScreen} exact />
+      <Route path="/application" component={ApplicationScreen} exact />
+      <Route path="/track" component={TrackApplicationScreen} exact />
 
-          <Route path='/admin' component={AdminLoginScreen} exact />
-          <Route path='/forgot' component={AdminForgotPasswordScreen} exact />
-          <Route path='/change' component={AdminChangePasswordScreen} exact />
-          <Route path='/email-change/:creds' component={ChangePasswordScreen} />
-          <Route path='/dashboard' component={AdminDashboardScreen} exact />
-          <Route path='/model-listing' component={ModelListingScreen} exact />
-          <Route path='/faq' component={FaqScreen} exact />
-          
-          <Route path='/register' component={UserForm} />
+      <Route path="/admin" component={AdminLoginScreen} exact />
+      <Route path="/forgot" component={AdminForgotPasswordScreen} exact />
+      <Route path="/change" component={AdminChangePasswordScreen} exact />
+      <Route path="/email-change/:creds" component={ChangePasswordScreen} />
+      <Route path="/changepass" component={ResetPasswordScreen} />
+      <Route path="/dashboard" component={AdminDashboardScreen} exact />
+      <Route path="/model-listing" component={ModelListingScreen} exact />
+      <Route path="/faq" component={FaqScreen} exact />
+      <Route path="/register" component={UserForm} />
       {/* <Footer /> */}
     </Router>
   );
