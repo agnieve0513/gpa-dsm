9bb80fc0d5d45e4cc33ff0f01d2d91986b36278c
JobHanrheeCuta <job@xtendly.com>
2021-11-25 17:52:36 +0800

    Homescreen fixes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,27 +21,26 @@ function HomeScreen() {
                   </Col>
                   <Col md={4}></Col>
                 </Row>
-                    <Row>
-                      <Col md={1}></Col>
-                        <Col md={10}>
-                          <h2 className="mb-4 text-white" id="applyHead">APPLY FOR THE GPA ENERGY SENSE REBATE</h2>
-                        </Col>
-                      <Col md={1}></Col>
-
-                    </Row>
-                    <Row>
-                      <Col md={2}></Col>
-                      <Col md={8}>
-                        <p className="text-justify mb-4 text-white" id="applyCaption">
-                          Before you submit your rebate application please be sure to
-                          have all documents and information ready. If you have any questions
-                          you may contact us at <b>(671)647-5787 /8/9</b> or email us at <b>energysense@gpagwa.com</b>,
-                          visit our webpage at <a className="text-white" href="http://guampowerauthority.com/gpa_authority/EnergySense/es-home.html">
-                          <u>Energy Sense Website</u></a>
-                        </p>
-                      </Col>
-                      <Col md={2}></Col>
-                    </Row>
+                <Row>
+                  <Col md={1}></Col>
+                    <Col md={10}>
+                      <h2 className="mb-4 text-white" id="applyHead">APPLY FOR THE GPA ENERGY SENSE REBATE</h2>
+                    </Col>
+                  <Col md={1}></Col>
+                </Row>
+                <Row>
+                  <Col md={2}></Col>
+                  <Col md={8}>
+                    <p className="text-justify mb-4 text-white" id="applyCaption">
+                      Before you submit your rebate application please be sure to
+                      have all documents and information ready. If you have any questions
+                      you may contact us at <b>(671)647-5787 /8/9</b> or email us at <b>energysense@gpagwa.com</b>,
+                      visit our webpage at <a className="text-white" href="http://guampowerauthority.com/gpa_authority/EnergySense/es-home.html">
+                      <u>Energy Sense Website</u></a>
+                    </p>
+                  </Col>
+                  <Col md={2}></Col>
+                </Row>
                 <Link to={`/application`} className="btn btn-success btn-lg px-5 mb-4" id="applyOnlineBtn">
                   <h4 className="mb-0 p-1">Apply Online</h4>
                 </Link>
@@ -51,11 +50,11 @@ function HomeScreen() {
                   <a className="text-white" href="http://guampowerauthority.com/gpa_authority/EnergySense/es-home.html">
                     <u>View Approved Model Listing</u>
                   </a>
-                  </p>
+                </p>
                 <p className="mb-3 text-white"><Link to={`/faq`} className="text-white"><u>Online Rebate Requirements and FAQ</u></Link></p>
                 <br />
-                <Row>
-                    <Col className="text-center">
+                <Row className="fixed-bottom pb-4">
+                    <Col>
                         <small className="text-secondary">Energy Sense Rebate Program <br/>
                         Copyright &copy; 2020 GPA Powered By Xtendly</small>
                     </Col>
