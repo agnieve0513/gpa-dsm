ed6aaa3273b1a224c26183f67beacd6112f9f38b
JobHanrheeCuta <job@xtendly.com>
2021-12-06 15:37:55 +0800

    track application page updates

--- src/screens/TrackApplicationScreen.js
+++ src/screens/TrackApplicationScreen.js
@@ -82,86 +82,89 @@ function ApplicationScreen() {
     }
 
     return (
-        <>
+        <Container className="m-0 p-0" fluid>
             <CustomerHeader />
-            <Row>
-                <Col md={4}></Col>
-                <Col md={4} className="mt-5">
+            <Row className="px-0 mx-0 w-100">
+                <Col className="px-0" xl={4} md={2} xs={'auto'} />
+                <Col className="mt-4">
                     {
                         isSearch?
                         clickTrack?
                             track_application ?
                                 track_application.table ?
                                 track_application.table.length > 0?
-                                <Container>
-                                <span className="text-muted float-start mr-2" style={{marginRight:"5px", marginTop:"-3px"}}> 
-                                <b className="mr-2">Step {(stages.find((p) => p.stage === track_application.table[0].Stage).id+1)} of 5 </b>
-                                    </span>
-                                    <ProgressBar className="ml-2 mb-3" variant="success" now={(stages.find((p) => p.stage === track_application.table[0].Stage).percent)} />
-                                    
-
-                                    <p className="text-muted">Date Applied: <b className="float-end">{track_application.table[0].Application_Date} </b></p>
-                                    <p className="text-muted">Account Number: <b className="float-end" > *******{track_application.table[0].Account_no.slice(track_application.table[0].Account_no.length - 3) } </b> </p>
-                                    <p className="text-muted">System Type:  <b className="float-end"> {track_application.table[0].System_Type} </b> </p>
-                                    <p className="text-muted mb-3">Status: <b className="float-end">{track_application.table[0].Status } </b></p>
-                                    
-                                    <div class="d-grid gap-2 mt-3">
-                                        <button style={{borderRadius:"0.5rem"}} className="btn btn-success px-5 py-2" id="submitbtn" onClick={()=> resetHandler()}><b>TRACK NEW APPLICATION</b></button>
-                                    </div>
+                                <Container className="mb-2">
+                                    <h5 className="text-center text-info fs-3" id="trackTitle">TRACK YOUR APPLICATION</h5>
+                                    <Row className="px-0 mb-4 mx-0 d-flex align-items-center w-100">
+                                        <Col className="px-0">
+                                            <b className="px-0 d-flex mx-0" id="trackStep">Step {(stages.find((p) => p.stage === track_application.table[0].Stage).id+1)} of 5 </b>
+                                        </Col>
+                                        <Col className="px-0" id="colBar">
+                                            <ProgressBar className="px-0" id="trackBar" variant="success" now={(stages.find((p) => p.stage === track_application.table[0].Stage).percent)} />
+                                        </Col>
+                                    </Row>
+                                    <Row className="px-0 mb-5">
+                                        <p className="text-muted mb-3">Date Applied: <b className="float-end">{track_application.table[0].Application_Date} </b></p>
+                                        <p className="text-muted mb-3">Account Number: <b className="float-end" > *******{track_application.table[0].Account_no.slice(track_application.table[0].Account_no.length - 3) } </b> </p>
+                                        <p className="text-muted mb-3">System Type:  <b className="float-end"> {track_application.table[0].System_Type} </b> </p>
+                                        <p className="text-muted mb-3">Status: <b className="float-end">{track_application.table[0].Status } </b></p>
+                                    </Row>
+                                    <Row className="px-0">
+                                        <button style={{borderRadius:"0.5rem"}} className="btn btn-success py-3 mt-5 fw-normal" id="submitbtn" onClick={()=> resetHandler()}>
+                                            <b className="trackButtonText">TRACK NEW APPLICATION</b>
+                                        </button>
+                                    </Row>
                                 </Container>:
                                 <>
                                     Ambot
                                 </>
                                 :<>
-                                <p>No Application was Found.</p>
-                                <div class="d-grid gap-2 mt-5">
-                                    <button style={{borderRadius:"0.5rem"}} className="btn btn-success px-5 py-2" id="submitbtn" onClick={()=> resetHandler()}><b>TRACK NEW APPLICATION</b></button>
-                                </div>
+                                    <p>No Application was Found.</p>
+                                    <div class="d-grid gap-2 mt-5">
+                                        <button style={{borderRadius:"0.5rem"}} className="btn btn-success py-3 mt-5" id="submitbtn" onClick={()=> resetHandler()}>
+                                            <b className="trackButtonText">TRACK NEW APPLICATION</b>
+                                        </button>
+                                    </div>
                                 </>
                             :<>
                                 <p>No Application was Found.</p>
                                 <div class="d-grid gap-2 mt-5">
-                                    <button style={{borderRadius:"0.5rem"}} className="btn btn-success px-5 py-2" id="submitbtn" onClick={()=> resetHandler()}><b>TRACK NEW APPLICATION</b></button>
+                                    <button style={{borderRadius:"0.5rem"}} className="btn btn-success py-3 mt-5" id="submitbtn" onClick={()=> resetHandler()}>
+                                        <b className="trackButtonText">TRACK NEW APPLICATION</b>
+                                    </button>
                                 </div>
                             </>
                         :''
                         :
                         <Container>
-                        <h4 className="text-center text-info mb-5">TRACK YOUR APPLICATION</h4>
-                        <p>ENTER YOUR CONTROL NUMBER</p>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                        type='text'
-                                        placeholder=''
-                                        onChange={(e)=>setControlNo(e.target.value)}
-                                        value={control_no}
-                                        required
-                                    />
-
+                            <h4 className="text-center text-info mb-5">TRACK YOUR APPLICATION</h4>
+                            <p>ENTER YOUR CONTROL NUMBER</p>
+                            <InputGroup className="mb-3">
+                                <Form.Control
+                                            type='text'
+                                            placeholder=''
+                                            onChange={(e)=>setControlNo(e.target.value)}
+                                            value={control_no}
+                                            required
+                                        />
                             </InputGroup>
                             <div class="d-grid gap-2">
                                 <button className="btn btn-success px-5 py-2" id="submitbtn" onClick={()=> trackApplicationHandler()}><b>SUBMIT</b></button>
                             </div>
                         </Container>
                     }
-                    
-
+                    <Link to={`/`} className="text-success mx-auto px-5">
+                            <h4 className="text-center fs-5" id="trackBackBtn">Back to Homepage</h4>
+                    </Link>
+                    <Row>
+                        <br/>
+                        <small className="text-secondary position-absolute text-center" id="trackFooter">Energy Sense Rebate Program <br/>
+                        Copyright &copy; 2020 GPA Powered By Xtendly</small>
+                    </Row>                    
                 </Col>
-                <Col md={4} style={{display:"none"}}></Col>
+                <Col className="px-0" xl={4} md={2} xs={'auto'} />
             </Row>
-         
-            <Row className="">
-                <Col md={2} style={{display:"none"}}></Col>
-                <Col md={8}>
-                    <div className="d-flex">
-                        <Link to={`/`} className="text-success mx-auto px-5">
-                            <h4 className="text-center fs-4">BACK TO GPA HOMEPAGE</h4>
-                        </Link>
-                    </div>
-                </Col>
-                <Col md={2} style={{display:"none"}}></Col>
-            </Row>
-        </>
+        </Container>
     )
 }
 
